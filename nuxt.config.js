5196cc63ccb9cbcde24a29780bc8482845ac3aec
ollie <tribe@myself.com>
2021-04-15 20:19:16 +0100

    feat: ✨ add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,31 @@ export default {
         content: 'https://plchldr.co/i/1200x630?text=o/&bg=000&fc=fff',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
